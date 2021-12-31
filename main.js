24ce37c2106b36725a8f7de557505f7add7b58a6
Tragislav Work PC <s.dykomenko@gmail.com>
2021-12-31 08:35:35 +0300

    added create and delete methods

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ const select = container.querySelector('.visually-hidden');
 class ExtendedSelect {
     constructor(select) {
         this.class = select.className;
-        this.options = [...select.children];
     }
 
     watch() {
-        console.log(this.options);
+        console.log(select.children);
+    }
+
+    delete(value) {
+        select.remove(value);
+    }
+
+    new(content) {
+        let newOption = document.createElement('option');
+        newOption.value = select.children.length;
+        newOption.innerHTML = content;
+        select.appendChild(newOption);
     }
 }
 
