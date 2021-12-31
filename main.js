7fc827b32143785c560c332cab55d4b581723443
Tragislav Work PC <s.dykomenko@gmail.com>
2021-12-31 09:46:33 +0300

    began to bring the component to the desired form

--- main.js
+++ main.js
@@ -3,9 +3,28 @@
 const container = document.querySelector('.container');
 const select = container.querySelector('.visually-hidden');
 
+// Extended Select
 class ExtendedSelect {
-    constructor(select) {
-        this.class = select.className;
+    constructor(select, labelText) {
+        this.select = select;
+        // this.class = this.select.className;
+        this.labelText = labelText;
+        this.buttonText = `Посмотреть выбранное`;
+        this.select.setAttribute('size', 1);
+
+        this.mainDiv = document.createElement('div');
+        this.titleDiv = document.createElement('div');
+        this.label = document.createElement('h3');
+        this.btn = document.createElement('button');
+
+        this.label.innerHTML = this.labelText;
+        this.btn.innerHTML = this.buttonText;
+
+        this.mainDiv.appendChild(this.titleDiv);
+        this.titleDiv.appendChild(this.label);
+        this.titleDiv.appendChild(this.btn);
+        this.mainDiv.appendChild(this.select);
+        container.appendChild(this.mainDiv);
     }
 
     watch() {
@@ -24,4 +43,6 @@ class ExtendedSelect {
     }
 }
 
-const aaa = new ExtendedSelect(select);
+const aaa = new ExtendedSelect(select, 'Реализуемые товары');
+
+aaa.btn.addEventListener('click', () => alert('Hello World!'));
